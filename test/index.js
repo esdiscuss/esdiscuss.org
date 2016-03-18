137fc9d05da9271d6c21a0f126fc20ae39d3b493
Forbes Lindesay <forbes@lindesay.co.uk>
2016-03-18 01:42:00 +0000

    Increase test timeouts to 60s per request

--- test/index.js
+++ test/index.js
@@ -25,7 +25,7 @@ function path(path, statusCode, fn) {
         .then(function (res) {
           assert.equal(res.statusCode, statusCode);
         });
-    }, '20s');
+    }, '60s');
     if (typeof fn === 'function') {
       fn(req);
     }
